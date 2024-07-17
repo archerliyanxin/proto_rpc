779b393d3ecaf1189671d4aae635452845aa5250
liyanxin <liyanxin@gitlab.fhzny.com>
2024-07-17 17:23:31 +0800

    add: buffer

--- network/include/Buffer.h
+++ network/include/Buffer.h
@@ -1,6 +1,31 @@
 #pragma once
+#include "vector"
+#include "unistd.h"
 namespace network{
 class Buffer{
+public:
+    static const size_t kCheapPrepend = 8;
+    static const size_t kInitSize = 1024;
 
+    explicit Buffer(size_t initSize = kInitSize)
+        : buffer_(kCheapPrepend + initSize)
+        , readerIndex_(kCheapPrepend)
+        , writerIndex_(kCheapPrepend)
+    {}
+
+    size_t readableBytes() const {
+        return writerIndex_ - readerIndex_;
+    }
+
+    size_t writeableBytes () const{
+        return buffer_.size() - writerIndex_;
+    }
+
+
+
+private:
+    std::vector<char> buffer_;
+    size_t readerIndex_;
+    size_t writerIndex_;
 };
 }
