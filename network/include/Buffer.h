aa97cd50300e9a46a59842b77ae9e5dc1ef73510
liyanxin <liyanxin@gitlab.fhzny.com>
2024-07-18 12:40:34 +0800

    add: buffer

--- network/include/Buffer.h
+++ network/include/Buffer.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "vector"
 #include "unistd.h"
+#include "string"
+#include "cassert"
+#include "algorithm"
 namespace network{
 class Buffer{
 public:
@@ -18,7 +21,81 @@ public:
     }
 
     size_t writeableBytes () const{
+        int k = buffer_.capacity();
+        (void)k;
         return buffer_.size() - writerIndex_;
+    }
+
+    size_t prePendableBytes() const {
+        return readerIndex_;
+    }
+
+    void retrieve(size_t len){
+        if(len < readableBytes()){
+            readerIndex_ += len;
+        }else{
+            retrieveAll();
+        }
+    }
+
+    void retrieveAll(){
+        readerIndex_ = writerIndex_ = kCheapPrepend;
+    }
+
+    std::string retrieveAsString(size_t len){
+        std::string result(peek(), len);
+        retrieve(len);
+        return result;
+    }
+
+
+// 缓冲区可读数据的起始地址
+    const char * peek(){
+        return begin() + readerIndex_;
+    }
+
+    char *beginWrite(){
+        return begin() + writerIndex_;
+    }
+
+    const char *beginWrite() const{
+        return begin() + writerIndex_;
+    }
+
+    void ensureWriteableBytes(size_t len){
+        if(writeableBytes() < len){
+            makeSpace(len);
+        }
+    }
+
+    void append(const char* data, size_t len){
+        ensureWriteableBytes(len);
+        std::copy(data, data+len, beginWrite());
+    }
+
+    ssize_t readFd(int fd, int *saveErrno);
+private:
+    char *begin(){
+        // iterator operator *()
+        return &*buffer_.begin();
+    }
+
+    const char *begin() const {
+        // iterator operator *()
+        return  &*buffer_.begin();
+    }
+
+    void makeSpace(size_t len){
+        // ausme writeIndex_ not change
+        if(writeableBytes() + prePendableBytes() < len + kCheapPrepend){
+            buffer_.resize(writerIndex_ + len);
+        } else{
+            size_t readable = readableBytes();
+            std::copy(begin() + readerIndex_, begin() + writerIndex_ , begin() + kCheapPrepend);
+            writerIndex_ = kCheapPrepend + readable;
+            readerIndex_ = kCheapPrepend;
+            assert(readable == readableBytes());
+        }
     }
 
 
--- network/src/Buffer.cpp
+++ network/src/Buffer.cpp
@@ -1,1 +1,27 @@
 #include "Buffer.h"
+#include "sys/uio.h"
+namespace network{
+    ssize_t Buffer::readFd(int fd, int *saveErrno){
+        char extbuf[1024*64];
+
+        struct iovec vec[2];
+        int writeable = writeableBytes();
+        vec[0].iov_base = beginWrite();
+        vec[0].iov_len = writeable;
+
+        vec[1].iov_base = extbuf;
+        vec[1].iov_len = sizeof extbuf;
+
+        int iovcnt = (writeable < sizeof extbuf) ? 2 : 1;
+        ssize_t n = readv(fd,   vec, iovcnt);
+        if(n < 0){
+            *saveErrno = errno;
+        }else if(n < writeable){
+            writerIndex_ += n;
+        } else{
+            writerIndex_ = buffer_.size();
+            append(extbuf, n - writeable);
+        }
+        return n;
+    }
+}
